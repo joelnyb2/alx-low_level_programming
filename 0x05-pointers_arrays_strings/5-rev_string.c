83aeb9d6c35d939a5155a1c54aafccb6bd31adf3
joelnyb2 <joelnyb10m2@gmail.com>
2023-03-03 11:48:57 -0800

    hot fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,18 +9,19 @@ void rev_string(char *s)
 	int i;
 	int len = 0;
 	char tmp;
+	int len2;
 
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 
-
+	len2 = len - 1;
 	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
-		s[i] = s[len - 1];
-		s[len - 1] = tmp;
-		(len - 1)--;
+		s[i] = s[len2];
+		s[len2] = tmp;
+		len2--;
 	}
 }

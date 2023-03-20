e5440d5630e3441fe2e2e1af9c359e249919bfd3
joelnyb2 <joelnyb10m2@gmail.com>
2023-03-20 05:15:42 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - concatentate and dynamically allocate
+ * @s1: string one
+ * @s2: string two
+ * Return: address of new allocation
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int size1 = 0;
+	int size2 = 0;
+	int SIZE;
+	int i;
+	char *ptr;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	while (s1[size1] != '\0')
+	{
+		size1++;
+	}
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+	SIZE = size1 + size2;
+	ptr = (char *)malloc((SIZE + 1) * sizeof(char));
+
+	for (i = 0; i < size1; i++)
+	{
+		*(ptr + i) = s1[i];
+	}
+	for (i = size1; i < SIZE; i++)
+	{
+		*(ptr + i) = s2[i - size1];
+	}
+	return (ptr);
+
+}

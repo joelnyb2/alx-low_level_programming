7178cd6d6a730fbdd6df9924b675bfd5781f916b
joelnyb2 <joelnyb10m2@gmail.com>
2023-03-21 06:14:47 -0700

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,7 @@ char *str_concat(char *s1, char *s2)
 	int SIZE;
 	int i;
 	char *ptr;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -40,7 +40,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		*(ptr + i) = s2[i - size1];
 	}
-	
+
 
 	return (ptr);
 
